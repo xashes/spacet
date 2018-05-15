d593c6a79c598bdd0d6c0221f0b8b8d666a26b9e
xashes <kellerx0319@gmail.com>
2018-05-15 18:56:17 +0800

    display chart with brush, turnover, and macd

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,10 @@
 from app import app
 from flask import render_template, redirect, url_for
 from app.forms import SymbolForm, RecordForm
+from app.charts import brush
+from tdata import local
 
+REMOTE_HOST = "https://pyecharts.github.io/assets/js"
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
@@ -14,9 +17,16 @@ def index():
 @app.route('/chart/<symbol>', methods=['GET', 'POST'])
 def chart(symbol):
     form = SymbolForm()
+    data = local.daily(symbol)
+    brush_chart = brush(data)
+    context = dict(
+        myechart=brush_chart.render_embed(),
+        host=REMOTE_HOST,
+        script_list=brush_chart.get_js_dependencies(),
+    )
     if form.validate_on_submit():
         return redirect(f'/chart/{form.symbol.data}')
-    return render_template('chart.html', form=form, symbol=symbol)
+    return render_template('chart.html', form=form, **context)
 
 @app.route('/record', methods=['GET', 'POST'])
 def record():
