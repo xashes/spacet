3e796ac6d60d5e3265fab6c3e9d2dfe2e12e596c
xashes <kellerx0319@gmail.com>
2018-05-24 17:22:31 +0800

    delete sql related code

--- config.py
+++ config.py
@@ -5,4 +5,3 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hardtoguess'
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         'DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
