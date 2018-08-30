a985c326bd19137d2f149475b550a921c1a73993
xashes <xashes@outlook.com>
2018-08-30 11:38:14 +0800

    use pyecharts 0.5.5 to fix a render_embed bug

--- app/charts.py
+++ app/charts.py
@@ -1,56 +1,30 @@
 from pyecharts import Kline, Bar, Line
-from pyecharts import Grid, Overlap, Page
-from app.zen import hist_sum
-
-
-def kline(data):
-    kline = Kline()
-    kline.add(
-        'Kline',
-        data.index,
-        data.loc[:, ['open', 'close', 'low', 'high']].values,
-        mark_line=['max', 'min'],
-        mark_line_valuedim=['highest', 'lowest'],
-        is_datazoom_show=True,
-        datazoom_xaxis_index=[0, 1],
-        datazoom_type='both',
-        tooltip_axispointer_type='cross',
-        is_more_utils=True,
-    )
-
-    turnover = Bar()
-    turnover.add(
-        '',
-        data.index,
-        data['turnover'].values / pow(10, 9),
-        mark_line=['max', 'min'],
-    )
-
-    grid = Grid(width='100%', height=900)
-    grid.add(kline, grid_bottom='40%')
-    grid.add(turnover, grid_top='65%')
-    return grid
-
+from pyecharts import Grid, Overlap
+from tdata import feature
 
 def brush(data):
+    data = feature.full_data(data)
+    print(data.tail())
+
     kline = Kline()
     kline.add(
         'Kline',
         data.index,
         data.loc[:, ['open', 'close', 'low', 'high']].values,
-        mark_line=['max', 'min'],
-        mark_line_valuedim=['highest', 'lowest'],
-        is_datazoom_show=True,
-        datazoom_xaxis_index=[0, 1, 2],
-        datazoom_type='both',
-        tooltip_axispointer_type='cross',
-        is_more_utils=True,
+        # mark_line=['max', 'min'],
+        # mark_line_valuedim=['highest', 'lowest'],
+        # is_datazoom_show=True,
+        # datazoom_xaxis_index=[0, 1, 2],
+        # datazoom_type='both',
+        # tooltip_axispointer_type='cross',
+        # is_more_utils=True,
     )
+
     brush = Line()
     brush.add(
         'Brush',
         data.index,
-        data.endpoint.values,
+        data.endpoint
     )
     overlap = Overlap()
     overlap.add(kline)
@@ -60,7 +34,7 @@ def brush(data):
     turnover.add(
         '',
         data.index,
-        data.brush_amount.values / pow(10, 6),
+        data.turnover.values / pow(10, 6),
         mark_line=['max', 'min']
     )
 
@@ -68,7 +42,7 @@ def brush(data):
     macd.add(
         '',
         data.index,
-        data.hist_sum.values,
+        data.macdhist.values,
         mark_line=['max', 'min'],
     )
 
@@ -77,4 +51,4 @@ def brush(data):
     grid.add(turnover, grid_top='65%', grid_bottom='20%')
     grid.add(macd, grid_top='85%')
 
-    return grid
+    return overlap
